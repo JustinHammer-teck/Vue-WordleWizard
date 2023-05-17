67f7d5077e83651d70cc1b55f292a3fff2a82e2a
Nguyen Dinh Nhat Tai <nguyendinhnhattai.work@gmail.com>
2023-05-18 00:36:23 +0700

    add function to fetch data from API

---
+++ src/services/processWordsService.ts
@@ -0,0 +1,24 @@
+export default async function processWordsService(resourceUrl: string, data: any) {
+    const response = await fetch(resourceUrl, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if( response.status != 201 ){
+                throw response.status;
+            }else{
+                return response.json();
+            }
+        })
+        .then(err => {
+            console.log();
+        });
+
+    return response
+}
